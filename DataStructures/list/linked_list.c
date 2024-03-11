e7696be79ca2fbf949aa7c049137b5ef7aa72bb7
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-11 18:44:20 +0100

    Write code to insert a node at any position in a linked list

--- DataStructures/list/linked_list.c
+++ DataStructures/list/linked_list.c
@@ -12,14 +12,39 @@ struct Node {
 
 struct Node* head; // global variable; can be accessed anywhere
 
-void insert(int x) { /* insert at head */
+
+void insert_at_head(int x) { /* insert x at head */
     struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
     (*temp).data = x;
     (*temp).next = head;
     head = temp;
 }
 
-void print() {
+
+void insert(int val, int position) { /* insert val at position */
+    // create the new node to be added
+    struct Node* temp1 = (struct Node*)malloc(sizeof(struct Node*));
+    temp1->data = val;
+    temp1->next = NULL;
+    
+    // case 1: list is empty; node is inserted at the head of the list
+    if(position == 1) {
+        temp1->next = head;
+        head = temp1;
+        return;
+    }
+   
+    // case 2: node is inserted at any other position in the list
+    struct Node* temp2 = head;
+    for(int i = 0; i < position - 2; ++i) { /* traverse the list to point at position - 1 */ 
+        temp2 = temp2->next;
+    }
+    temp1->next = temp2->next;  // set next field of newly created node to point to the node currently at position + 1
+    temp2->next = temp1;        // set next field of node at position - 1 to point to the newly created node
+}
+
+
+void print_list() {
     struct Node* temp = head;
     printf("List is: ");
     while (temp != NULL) {
@@ -42,7 +67,8 @@ void main() {
     for(i = 0; i < n; i++) {
         printf("Enter the number \n");
         scanf("%d", &x);
-        insert(x);
-        print();
+        // insert_at_head(x);
+        insert(x, i + 1);
+        print_list();
     }
 }
