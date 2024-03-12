560942ab993df4c8e67f42497f120021bfacfd8e
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-12 14:46:29 +0100

    refactor: Separate list deletion related code into different files

--- DataStructures/list/delete_node_by_value.c
+++ DataStructures/list/delete_node_by_value.c
@@ -1,9 +1,9 @@
 /*
-Implementation of linked list in C/C++
+ C implementation of deletion of a node given its position in a linked list
 */
 
 #include <stdio.h>
-#include <stdlib.h> // Include stdlib.h for malloc
+#include <stdlib.h> 
 
 struct Node {
     int data;
@@ -13,34 +13,43 @@ struct Node {
 struct Node* head; // global variable; can be accessed anywhere
 
 
-void insert_at_head(int x) { /* insert x at head */
-    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-    (*temp).data = x;
-    (*temp).next = head;
-    head = temp;
+void delete_pos(int position) { /* delete node at position */
+    struct Node* temp1 = head;
+    struct Node* temp2;
+
+
+    if (position == 1) {
+        head = temp1->next;
+        free(temp1);
+        return;
+    }
+
+    for(int i = 0; i < position - 2; ++i) {
+        temp1 = temp1->next;
+    }
+    // temp1 points to node before delete position;
+    temp2 = temp1->next;        // node at delete position
+    temp1->next = temp2->next;  // node after delete position
+    free(temp2);                // delete temp2
 }
 
-
-void insert(int val, int position) { /* insert val at position */
-    // create the new node to be added
+void insert(int val, int position) { /* helper function to insert values into list for test */
     struct Node* temp1 = (struct Node*)malloc(sizeof(struct Node*));
     temp1->data = val;
     temp1->next = NULL;
     
-    // case 1: list is empty; node is inserted at the head of the list
     if(position == 1) {
         temp1->next = head;
         head = temp1;
         return;
     }
    
-    // case 2: node is inserted at any other position in the list
     struct Node* temp2 = head;
-    for(int i = 0; i < position - 2; ++i) { /* traverse the list to point at position - 1 */ 
+    for(int i = 0; i < position - 2; ++i) {  
         temp2 = temp2->next;
     }
-    temp1->next = temp2->next;  // set next field of newly created node to point to the node currently at position + 1
-    temp2->next = temp1;        // set next field of node at position - 1 to point to the newly created node
+    temp1->next = temp2->next;  
+    temp2->next = temp1;        
 }
 
 void print_list() {
@@ -66,8 +75,10 @@ void main() {
     for(i = 0; i < n; i++) {
         printf("Enter the number \n");
         scanf("%d", &x);
-        // insert_at_head(x);
         insert(x, i + 1);
         print_list();
     }
+    delete_pos(3);
+    putchar('\n');
+    print_list();
 }
